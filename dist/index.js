f98d4a1531b96f631f90da6f0b544aab97ba8615
NgirfanulMangali <metalqwerty0@gmail.com>
2025-09-21 23:11:45 -0700

    making read data and update data

--- dist/index.js
+++ dist/index.js
@@ -5,20 +5,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const data = [
     { id: 1, name: 'johnson', address: ' california' },
     { id: 2, name: 'robert', address: 'new york' }
 ];
-app.get('/user/:id', (req, res) => {
-    const id = parseInt(req.params.id); // string → number
-    const user = data.find(u => u.id === id); // find user by id
-    if (user) {
-        res.json(user);
-    }
-    else {
-        res.status(404).json({ message: 'User not found' });
-    }
+// read all data
+app.get('/', (req, res) => {
+    res.json(data);
+});
+// read specific data
+app.get('/data/:id', (req, res) => {
+    const specificId = Number(req.params.id);
+    const match = data.find(i => i.id === specificId);
+    if (!match)
+        return res.status(404).json({ message: 'error' });
+    res.json(match);
+});
+// update the data 
+app.put('/data/:id', (req, res) => {
+    const specificId = Number(req.params.id);
+    const { name, address } = req.body;
+    const match = data.find(i => i.id === specificId);
+    if (!match)
+        return res.status(404).json({ message: 'error' });
+    if (name)
+        match.name = name;
+    if (address)
+        match.address = address;
+    res.json(match);
 });
 app.listen(3000, () => {
-    console.log('🚀 Server running at http://localhost:3000');
+    console.log('Server started on port http://localhost:3000/');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 const app = express()
-
+app.use(express.json());
 interface User {
     id: number;
     name: string;
@@ -12,25 +12,43 @@ const data: User[] = [
 { id: 2, name:'robert', address:'new york'}
 ];
 
+// read all data
+app.get('/', (req: Request, res: Response) => {
+    res.json(data)
+})
 
-app.get('/user/:id', (req: Request, res: Response) => {
-    const id = parseInt(req.params.id); // string → number
-    const user = data.find(u => u.id === id); // find user by id
+// read specific data
+app.get('/data/:id', (req: Request, res: Response) => {
+    
+    const specificId = Number(req.params.id) 
+    const match: User | undefined  = data.find(i => i.id === specificId)
+    if(!match) return res.status(404).json({message: 'error'})
+    res.json(match)
+}
+)
 
-    if (user) {
-        res.json(user);
-    } else {
-        res.status(404).json({ message: 'User not found' });
-    }
-});
 
+// update the data 
+app.put('/data/:id', (req: Request, res: Response) => {
 
+    const specificId = Number(req.params.id)
+    const { name, address} = req.body
+    const match: User | undefined  = data.find(i => i.id === specificId)
+    if(!match) return res.status(404).json({message: 'error'})
+
+    if (name) match.name = name;
+    if (address) match.address = address;
+
+    res.json(match)
+
+})
 
 
 
 app.listen(3000, () => {
-  console.log('🚀 Server running at http://localhost:3000');
+  console.log('Server started on port http://localhost:3000/');
 });
+
 
 
 
