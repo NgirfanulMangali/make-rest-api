f17e587ce139f6ef45a4c6851f47a8b2e8adf4f7
NgirfanulMangali <metalqwerty0@gmail.com>
2025-09-24 01:54:24 -0700

    debug Prisma Migrate could not create the shadow database. Please make sure the database user has permission to create databases. Read more about the shadow database (and workarounds) at https://pris.ly/d/migrate-shadow

--- dist/index.js
+++ dist/index.js
@@ -5,47 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
-const data = [
-    { id: 1, name: 'johnson', address: ' california' },
-    { id: 2, name: 'robert', address: 'new york' }
-];
 // read all data
-app.get('/', (req, res) => {
-    res.json(data);
-});
+const buah = [{ id: 2, name: "eeee", address: "fffffff" }, { id: 1, name: "John Doe", address: "123 Main St" }];
 // read specific data
-app.get('/data/:id', (req, res) => {
-    const specificId = Number(req.params.id);
-    const match = data.find(i => i.id === specificId);
-    if (!match)
-        return res.status(404).json({ message: 'error' });
-    res.json(match);
+app.get('/buah', (req, res) => {
+    res.json(buah);
 });
-// update the data 
-app.put('/data/:id', (req, res) => {
-    const specificId = Number(req.params.id);
-    const { name, address } = req.body;
-    const match = data.find(i => i.id === specificId);
-    if (!match)
-        return res.status(404).json({ message: 'error' });
-    if (name)
-        match.name = name;
-    if (address)
-        match.address = address;
-    res.json(match);
-});
-// Create the Data
-app.post('/Users', (req, res) => {
-    const { name, address } = req.body;
-    // generate ID based on current array length
-    const id = data.length + 1;
-    // Create one new user
-    const newUser = { id, name, address };
-    // Save to array
-    data.push(newUser);
-    res.status(201).json(newUser);
-});
-app.listen(3000, () => {
-    console.log('Server started on port http://localhost:3000/');
+app.listen(4000, () => {
+    console.log('Server started on port http://localhost:4000/buah');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,40 @@
 import express, { Request, Response } from 'express';
 const app = express()
-app.use(express.json());
+app.use(express.json())
 interface User {
     id: number;
     name: string;
     address: string;
 }
 
-const data: User[] = [
-{ id: 1, name: 'johnson', address:' california'},
-{ id: 2, name:'robert', address:'new york'}
-];
 
 // read all data
-app.get('/', (req: Request, res: Response) => {
-    res.json(data)
-})
 
+const data: User[] = [{ id:1, name: "eeee", address: "fffffff"}, { id: 2, name: "John Doe", address: "123 Main St" }];
 // read specific data
-app.get('/data/:id', (req: Request, res: Response) => {
-    
-    const specificId = Number(req.params.id) 
-    const match: User | undefined  = data.find(i => i.id === specificId)
-    if(!match) return res.status(404).json({message: 'error'})
-    res.json(match)
-}
-)
-
-
-// update the data 
-app.put('/data/:id', (req: Request, res: Response) => {
-
-    const specificId = Number(req.params.id)
-    const { name, address} = req.body
-    const match: User | undefined  = data.find(i => i.id === specificId)
-    if(!match) return res.status(404).json({message: 'error'})
-
-    if (name) match.name = name;
-    if (address) match.address = address;
-
-    res.json(match)
-
-})
-
-// Create the Data
-app.post('/Users', (req: Request, res: Response) => {
-    const { name, address} = req.body
-     // generate ID based on current array length
-  const id = data.length + 1;
-
-  // Create one new user
-  const newUser: User = { id, name, address };
-
-  // Save to array
+app.get('/', (req: Request, res: Response) => {
+  res.json(data);
+});
+app.post('/newUser', (req: Request, res: Response) => {
+  const newUser: User = req.body;
   data.push(newUser);
+  res.json(data);
+});
 
-  res.status(201).json(newUser);
-})
+app.delete('/deleteUser/:id', (req: Request, res: Response) => {
+  const userId = parseInt(req.params.id);
+  const index = data.findIndex(user => user.id === userId);
+  if (index !== -1) {
+    data.splice(index, 1);
+    res.json({ message: 'User deleted successfully', data });
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
 
 
-
-
-app.listen(3000, () => {
-  console.log('Server started on port http://localhost:3000/');
+app.listen(5000, () => {
+  console.log('Server started on port http://localhost:5000');
 });
 
 
